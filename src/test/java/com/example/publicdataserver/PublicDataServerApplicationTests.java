886d0a8e0a90c967ea37b75b2b5d767504bfd48f
euics <pjh575157@gmail.com>
2024-04-06 00:49:49 +0900

    Initial Commit

---
+++ src/test/java/com/example/publicdataserver/PublicDataServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.publicdataserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicDataServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
