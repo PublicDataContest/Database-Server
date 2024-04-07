1e1610350baac5d1cc530a259e787f4a559db045
euics <pjh575157@gmail.com>
2024-04-07 16:23:16 +0900

    test: AWS PostgreSQL 연동 (#7)

--- src/test/java/com/example/publicdataserver/PublicDataServerApplicationTests.java
+++ src/test/java/com/example/publicdataserver/PublicDataServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.publicdataserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("production")
 class PublicDataServerApplicationTests {
 
     @Test
