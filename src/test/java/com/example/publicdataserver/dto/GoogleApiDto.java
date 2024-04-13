62aee9d81b38a6c3df0d7ca48a3c429f1183b796
euics <pjh575157@gmail.com>
2024-04-13 18:36:12 +0900

    test: Google Id - Google Place API 연동 (#13)

---
+++ src/test/java/com/example/publicdataserver/dto/GoogleApiDto.java
@@ -0,0 +1,49 @@
+package com.example.publicdataserver.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+public class GoogleApiDto {
+
+    @Getter
+    @ToString
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class GooglePlaceDetailsDto {
+        @JsonProperty("formatted_address")
+        private String formattedAddress;
+
+        @JsonProperty("name")
+        private String name;
+
+        @JsonProperty("rating")
+        private Integer rating;
+
+        private List<ReviewsInfo> reviews;
+
+        @Getter
+        @ToString
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        public static class ReviewsInfo {
+            @JsonProperty("author_name")
+            private String authorName;
+
+            @JsonProperty("profile_photo_url")
+            private String profilePhotoUrl;
+
+            @JsonProperty("rating")
+            private Integer rating;
+
+            @JsonProperty("relative_time_description")
+            private String relativeTimeDescription;
+
+            @JsonProperty("text")
+            private String text;
+        }
+    }
+}
---
+++ src/test/java/com/example/publicdataserver/dto/KakaoApiDto.java
@@ -0,0 +1,38 @@
+package com.example.publicdataserver.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.ToString;
+
+public class KakaoApiDto {
+
+    @Getter
+    @ToString
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class KakaoPlaceDetailsDto {
+        @JsonProperty("address_name")
+        private String addressName;
+
+        @JsonProperty("category_group_name")
+        private String categoryGroupName;
+
+        @JsonProperty("category_name")
+        private String categoryName;
+
+        @JsonProperty("phone")
+        private String phone;
+
+        @JsonProperty("place_name")
+        private String placeName;
+
+        @JsonProperty("place_url")
+        private String placeUrl;
+
+        @JsonProperty("x")
+        private String x;
+
+        @JsonProperty("y")
+        private String y;
+    }
+}
